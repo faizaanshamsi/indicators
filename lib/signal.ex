39d07921bc1b27867b01674646c26ab20414d57d
faizaanshamsi <faizaanshamsi@gmail.com>
2015-02-21 18:33:23 -0500

    refactor like a boss

--- lib/signal.ex
+++ lib/signal.ex
@@ -3,21 +3,12 @@ defmodule Signal do
   @doc """
   Checks if indicators have been hit
   """
-  def check(:sma, data) do
+  def check(name, data) when name == :ema or name == :sma do
     case Indicator.crossed(data[:moving_average], data[:previous_tick], data[:current_tick]) do
       {:false, direction} ->
         {:false}
       {:true, direction} ->
-        {:sma, data[:ticker], direction, data[:current_tick], "timestamp"}
-    end
-  end
-
-  def check(:ema, data) do
-    case Indicator.crossed(data[:moving_average], data[:previous_tick], data[:current_tick]) do
-      {:false, direction} ->
-        {:false}
-      {:true, direction} ->
-        {:ema, data[:ticker], direction, data[:current_tick], "timestamp"}
+        {name, data[:ticker], direction, data[:current_tick], "timestamp"}
     end
   end
 end
