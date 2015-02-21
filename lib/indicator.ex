cd7f58ecd06dc2f4f2d5b9a2fda9d4084041fd37
faizaanshamsi <faizaanshamsi@gmail.com>
2015-02-21 17:40:28 -0500

    move everything to indicator module

--- lib/indicator.ex
+++ lib/indicator.ex
@@ -1,2 +1,22 @@
 defmodule Indicator do
+  @doc """
+  Checks if indicators have been hit
+  """
+
+  def crossed(moving_average, previous_tick, current_tick) do
+    cond do
+      previous_tick == current_tick && current_tick == moving_average ->
+        {:true, :none}
+      previous_tick == current_tick && current_tick != moving_average ->
+        {:false, :none}
+      previous_tick <= moving_average && current_tick >= moving_average ->
+        {:true, :up}
+      current_tick <= moving_average && previous_tick >= moving_average ->
+        {:true, :down}
+      current_tick < moving_average && previous_tick < current_tick ->
+        {:false, :up}
+      true ->
+        {:false, :down}
+    end
+  end
 end
--- test/indicator_test.exs
+++ test/indicator_test.exs
@@ -1,7 +1,68 @@
 defmodule IndicatorTest do
-  use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  use ExUnit.Case, async: true
+
+  test "Simple Moving Average is crossed from a low to high price movement" do
+    assert Indicator.crossed(100.0, 50.0, 150.0) == {:true, :up}
+  end
+
+  test "Simple Moving Average is crossed from a high to low price movement" do
+    assert Indicator.crossed(100.0, 150.0, 50.0) == {:true, :down}
+  end
+
+  test "Simple Moving Average is reached from low to high price movement" do
+    assert Indicator.crossed(100.0, 50.0, 100.0) == {:true, :up}
+  end
+
+  test "Simple Moving Average is reached from high to low price movement" do
+    assert Indicator.crossed(100.0, 150.0, 100.0) == {:true, :down}
+  end
+
+  test "Simple Moving Average is not crossed, price moves low to high" do
+    assert Indicator.crossed(100.0, 50.0, 99.99) == {:false, :up}
+  end
+
+  test "Simple Moving Average is not crossed, price moves high to low" do
+    assert Indicator.crossed(100.0, 150.0, 100.01) == {:false, :down}
+  end
+
+  test "Simple Moving Average equal unchanged price" do
+    assert Indicator.crossed(100.00, 100.0, 100.0) == {:true, :none}
+  end
+
+  test "Price does not change, does not equal Simple Moving Average" do
+    assert Indicator.crossed(100.00, 10.0, 10.0) == {:false, :none}
+  end
+
+  test "Exponential Moving Average is crossed from a low to high price movement" do
+    assert Indicator.crossed(100.0, 50.0, 150.0) == {:true, :up}
+  end
+
+  test "Exponential Moving Average is crossed from a high to low price movement" do
+    assert Indicator.crossed(100.0, 150.0, 50.0) == {:true, :down}
+  end
+
+  test "Exponential Moving Average is reached from low to high price movement" do
+    assert Indicator.crossed(100.0, 50.0, 100.0) == {:true, :up}
+  end
+
+  test "Exponential Moving Average is reached from high to low price movement" do
+    assert Indicator.crossed(100.0, 150.0, 100.0) == {:true, :down}
+  end
+
+  test "Exponential Moving Average is not crossed, price moves low to high" do
+    assert Indicator.crossed(100.0, 50.0, 99.99) == {:false, :up}
+  end
+
+  test "Exponential Moving Average is not crossed, price moves high to low" do
+    assert Indicator.crossed(100.0, 150.0, 100.01) == {:false, :down}
+  end
+
+  test "Exponential Moving Average equal unchanged price" do
+    assert Indicator.crossed(100.00, 100.0, 100.0) == {:true, :none}
+  end
+
+  test "Price does not change, does not equal Exponential Moving Average" do
+    assert Indicator.crossed(100.00, 10.0, 10.0) == {:false, :none}
   end
 end
