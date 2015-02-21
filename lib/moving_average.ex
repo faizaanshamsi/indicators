726f0ca9a5ae43e7a5409984800167ea36ab6248
faizaanshamsi <faizaanshamsi@gmail.com>
2015-02-21 17:33:20 -0500

    moving average calculations working

--- lib/moving_average.ex
+++ lib/moving_average.ex
@@ -3,11 +3,28 @@ defmodule MovingAverage do
   Checks indicators related to moving averages
   """
 
-  def simple(moving_average, current_tick, previous_tick) do
-
+  def simple(moving_average, previous_tick, current_tick) do
+    crossed(moving_average, previous_tick, current_tick)
   end
 
-  def exponential(moving_average, current_tick, previous_tick) do
+  def exponential(moving_average, previous_tick, current_tick) do
+    crossed(moving_average, previous_tick, current_tick)
+  end
 
+  defp crossed(moving_average, previous_tick, current_tick) do
+    cond do
+      previous_tick == current_tick && current_tick == moving_average ->
+        {:true, :none}
+      previous_tick == current_tick && current_tick != moving_average ->
+        {:false, :none}
+      previous_tick <= moving_average && current_tick >= moving_average ->
+        {:true, :up}
+      current_tick <= moving_average && previous_tick >= moving_average ->
+        {:true, :down}
+      current_tick < moving_average && previous_tick < current_tick ->
+        {:false, :up}
+      true ->
+        {:false, :down}
+    end
   end
 end
--- test/moving_average_test.exs
+++ test/moving_average_test.exs
@@ -4,50 +4,66 @@ defmodule MovingAverageTest do
   use ExUnit.Case, async: true
 
   test "Simple Moving Average is crossed from a low to high price movement" do
-    assert _ == {:true, :up}
+    assert M.simple(100.0, 50.0, 150.0) == {:true, :up}
   end
 
   test "Simple Moving Average is crossed from a high to low price movement" do
-    assert _ == {:true, :down}
+    assert M.simple(100.0, 150.0, 50.0) == {:true, :down}
   end
 
   test "Simple Moving Average is reached from low to high price movement" do
-    assert _ == {:true, :up}
+    assert M.simple(100.0, 50.0, 100.0) == {:true, :up}
   end
 
-  test "Simple Moving Average is crossed from high to low price movement" do
-    assert _ == {:true, :down}
+  test "Simple Moving Average is reached from high to low price movement" do
+    assert M.simple(100.0, 150.0, 100.0) == {:true, :down}
   end
 
   test "Simple Moving Average is not crossed, price moves low to high" do
-    assert _ = {:false, :up}
+    assert M.simple(100.0, 50.0, 99.99) == {:false, :up}
   end
 
   test "Simple Moving Average is not crossed, price moves high to low" do
-    assert _ = {:false, :down}
+    assert M.simple(100.0, 150.0, 100.01) == {:false, :down}
+  end
+
+  test "Simple Moving Average equal unchanged price" do
+    assert M.simple(100.00, 100.0, 100.0) == {:true, :none}
+  end
+
+  test "Price does not change, does not equal Simple Moving Average" do
+    assert M.simple(100.00, 10.0, 10.0) == {:false, :none}
   end
 
   test "Exponential Moving Average is crossed from a low to high price movement" do
-    assert _ == {:true, :up}
+    assert M.exponential(100.0, 50.0, 150.0) == {:true, :up}
   end
 
   test "Exponential Moving Average is crossed from a high to low price movement" do
-    assert _ == {:true, :down}
+    assert M.exponential(100.0, 150.0, 50.0) == {:true, :down}
   end
 
   test "Exponential Moving Average is reached from low to high price movement" do
-    assert _ == {:true, :down}
+    assert M.exponential(100.0, 50.0, 100.0) == {:true, :up}
   end
 
-  test "Exponential Moving Average is crossed from high to low price movement" do
-    assert _ == {:true, :down}
+  test "Exponential Moving Average is reached from high to low price movement" do
+    assert M.exponential(100.0, 150.0, 100.0) == {:true, :down}
   end
 
   test "Exponential Moving Average is not crossed, price moves low to high" do
-    assert _ = {:false, :up}
+    assert M.exponential(100.0, 50.0, 99.99) == {:false, :up}
   end
 
   test "Exponential Moving Average is not crossed, price moves high to low" do
-    assert _ = {:false, :down}
+    assert M.exponential(100.0, 150.0, 100.01) == {:false, :down}
+  end
+
+  test "Exponential Moving Average equal unchanged price" do
+    assert M.exponential(100.00, 100.0, 100.0) == {:true, :none}
+  end
+
+  test "Price does not change, does not equal Exponential Moving Average" do
+    assert M.exponential(100.00, 10.0, 10.0) == {:false, :none}
   end
 end
